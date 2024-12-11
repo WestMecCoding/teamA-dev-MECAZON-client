7d16acdebd9d018468fd8593784f39906718d2a3
Jacob Ploog <jploog066@west-mec.org>
2024-12-11 14:11:52 -0700

    logo added to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
 import styles from "../styles/Navbar.module.css";
+import { Link } from "react-router-dom";
+import Logo from "./logo.jsx";
+
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <Link to="/">Home</Link>
-      <Link to="/groceries">Groceries</Link>
     </nav>
   );
 }
