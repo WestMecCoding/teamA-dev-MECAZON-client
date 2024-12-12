c26f38a4652ed9017ffd51a542818ff0ac461c80
hfuent761 <hfuent761@westmec.org>
2024-12-12 14:19:52 -0700

    cats

--- src/components/Catagories.jsx
+++ src/components/Catagories.jsx
@@ -1,3 +1,13 @@
+import styles from "../styles/Catagories.module.css";
+
 export default function Catagories() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <a href="">Mens</a>
+      <a href="">Womens</a>
+      <a href="">Sports</a>
+      <a href="">Office Supplies</a>
+      <a href="">blahh</a>
+    </nav>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,23 @@
 import styles from "../styles/Navbar.module.css";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar.jsx";
+import Catagories from "./Catagories.jsx";
 import LogoLG from "./LogoLG.jsx";
 import LogoSM from "./LogoSM.jsx";
 import LogoMD from "./LogoMD.jsx";
 
 export default function Navbar() {
   return (
-    <nav className={styles.nav}>
-      <LogoLG />
-      <Link to="/">Home</Link>
-      <Link to="/">Products</Link>
-      <SearchBar />
-      <Link to="/">Sign-Up/Log-in</Link>
-      
-      <Link to="/">Cart</Link>
-    </nav>
+    <div>
+      <nav className={styles.nav}>
+        <LogoLG />
+        <Link to="/">Home</Link>
+        <Link to="/">Products</Link>
+        <SearchBar />
+        <Link to="/">Sign-Up/Log-in</Link>
+        <Link to="/">Cart</Link>
+      </nav>
+      <Catagories />
+    </div>
   );
 }
