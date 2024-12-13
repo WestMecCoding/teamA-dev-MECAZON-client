acdc91e681d421e5767dd3b664f64d7c06f0a1f1
Juancarlos Esquer Lopez <jesque708@west-mec.org>
2024-12-13 13:11:34 -0700

    switch to main branch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,37 @@
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
-  return <h1>Welcome to the Shopping Website</h1>;
+  return (
+  <>
+  <h1>Welcome to the Shopping Website</h1>
+  <div className={styles.container}>
+    <div className={styles.bigItem}>Big Div</div>
+    <div className={styles.smallContainer}>
+      <div className={styles.item}>Small Div 1</div>
+      <div className={styles.item}>Small Div 2</div>
+    </div>
+  </div>
+
+  <div className={styles.containerSide}>
+    <div className={styles.sideItem}>item 1</div>
+    <div className={styles.sideItem}>item 1</div>
+    <div className={styles.sideItem}>item 1</div>
+    <div className={styles.sideItem}>item 1</div>
+    <div className={styles.sideItem}>item 1</div>
+  </div>
+  {/* <div className={styles.container}>
+    <div className={`${styles.item} ${styles.main}`}>box 1</div>
+    <div className={styles.small-conatiner}>
+    <div className={`${styles.item} ${styles.item-2}`}>box 2</div>
+    <div className={styles.item}>box 3</div>
+    </div>
+  </div>
+  <div className={styles.side}>
+    <div className={styles.box}>side product</div>
+    <div className={styles.box}>side product</div>
+    <div className={styles.box}>side product</div>
+  </div> */}
+  </>
+);
 }
+
