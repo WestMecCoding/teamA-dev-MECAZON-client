36221804ba5f5a86a0429baa8e48a18c588b7b27
git <mgonza857@West-Mec.org>
2024-12-16 14:27:26 -0700

    product page

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -1,4 +1,5 @@
 import styles from "../styles/GroceryList.module.css";
+import { Link } from "react-router-dom";
 
 export default function GroceryList({ items }) {
   return (
@@ -6,7 +7,9 @@ export default function GroceryList({ items }) {
       {items.map((i) => (
         <div key={i.id} className={styles.item}>
           <img src="" alt="" className={styles.img} />
-          <h2>{i.name}</h2>
+          <h2>
+            <Link to="/product">{i.name}</Link>
+          </h2>
           <p>Category: {i.category}</p>
           <p>Price: {i.price}</p>
         </div>
