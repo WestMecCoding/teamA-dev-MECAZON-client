01a3581b4196050bc664f93855f1a6c2fe699a44
git <mgonza857@West-Mec.org>
2024-12-18 12:13:23 -0700

    final

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,23 +1,15 @@
 import styles from "../styles/Product.module.css";
 
-export default function Product({ items }) {
+export default function Product() {
   return (
     <>
       <div className={styles.body}>
-        <div className={styles.container}>
-          {items.map((i) => {
-            const image = new URL(i.imgSrc, import.meta.url).href;
-            return (
-              <>
-                <div className={styles.wrapper}>
-                  <div className={styles.smallbox}>small box 1</div>
-                  <div className={styles.smallbox}>small box 2</div>
-                  <div className={styles.smallbox}>small box 3</div>
-                </div>
-              </>
-            );
-          })}
+        <div className={styles.wrapper}>
+          <div className={styles.smallbox}>small box 1</div>
+          <div className={styles.smallbox}>small box 2</div>
+          <div className={styles.smallbox}>small box 3</div>
         </div>
+        <div className={styles.container}></div>
       </div>
 
       {/* <div className={styles.containerbig}>
@@ -26,3 +18,4 @@ export default function Product({ items }) {
     </>
   );
 }
+
